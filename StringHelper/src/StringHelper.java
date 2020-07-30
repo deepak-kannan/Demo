b1f9951aaca99a3b8023b42448f11ca47142ec82
digambarPa <digambar.patankar@tcs.com>
2020-07-30 14:54:39 +0530

    Auto Trigger Test

--- StringHelper/src/StringHelper.java
+++ StringHelper/src/StringHelper.java
@@ -7,4 +7,5 @@ String s3=new String("example");//creating java string by new keyword
 System.out.println(s1);  
 System.out.println(s2);  
 System.out.println(s3);  
+System.out.println("Auto Trigger Test");  
 }}  
