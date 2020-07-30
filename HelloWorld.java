9b63f8a8c3109b9ce9fdcd2b835a73c1b9025a44
deepak-kannan <55436375+deepak-kannan@users.noreply.github.com>
2020-07-30 14:28:35 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/*
-* The HelloWorld Java program
-* prints "Hello World!" on the computer screen
-*/
-class HelloWorld {
-  public static void main(String[] args) {
-   System.out.println("Hello, World!");
-  }
-}
